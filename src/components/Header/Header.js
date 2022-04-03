488cbd72c1d98b4f2d1014b690a1feda525f35df
zayanahmed925 <rjraju925@gmail.com>
2022-04-04 04:13:11 +0600

    header and route added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-
-        </div>
+        <nav className='header'>
+            <Link to='/home'>HOME</Link>
+            <Link to='/reviews'>REVIEWS</Link>
+            <Link to='/dashboard'>DASHBOARD</Link>
+            <Link to='/blogs'>BLOGS</Link>
+        </nav>
     );
 };
 
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>4004!!</h1>
+        </div>
+    );
+};
+
+export default NotFound;
